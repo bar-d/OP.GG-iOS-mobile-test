ae41683f067320a41d6e1750da37886c19f9ebf3
bard <schy2503@naver.com>
2023-01-04 00:38:51 +0900

    feat: CGRect extension 구현
    - 원형 프레임을 만들기 위한 circleRadius 연산 프로퍼티 구현

---
+++ Extension/CGRect+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  CGRect+Extension.swift
+//  OPGG-iOS-mobile-test
+//
+//  Created by bard on 2023/01/04.
+//
+
+import Foundation
+
+extension CGRect {
+    var circleRadius: CGFloat {
+        return self.size.width / 2
+    }
+}
