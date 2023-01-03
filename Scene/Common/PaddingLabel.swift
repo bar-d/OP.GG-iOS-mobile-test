e2603c9d00efc6e0ec4e18b4a908999aaa143129
bard <schy2503@naver.com>
2023-01-03 16:59:04 +0900

    feat: PaddingLabel 구현

---
+++ Scene/Common/PaddingLabel.swift
@@ -0,0 +1,41 @@
+//
+//  PaddingLabel.swift
+//  OPGG-iOS-mobile-test
+//
+//  Created by bard on 2023/01/03.
+//
+
+import UIKit
+
+class PaddingLabel: UILabel {
+    
+    // MARK: Properties
+    
+    private var padding: UIEdgeInsets
+    
+    // MARK: - Initializers
+    
+    init(top: CGFloat, left: CGFloat, bottom: CGFloat, right: CGFloat) {
+        self.padding = UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.padding = UIEdgeInsets()
+        super.init(frame: .zero)
+    }
+    
+    // MARK: - Methods
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+        
+        return contentSize
+    }
+}
