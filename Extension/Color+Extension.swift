35e9697896747c840e6b9872f8efaf9a3778bc2c
bard <schy2503@naver.com>
2023-01-03 16:36:47 +0900

    refactor: colorAsset 제거 및 UIColor extension 구현

---
+++ Extension/Color+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Color+Extension.swift
+//  OPGG-iOS-mobile-test
+//
+//  Created by bard on 2023/01/03.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    static let charcoalGrey = UIColor(red: 0.200, green: 0.220, blue: 0.235, alpha: 1)
+    static let coolGrey = UIColor(red: 0.596, green: 0.627, blue: 0.655, alpha: 1)
+    static let darkgrey = UIColor(red: 0.118, green: 0.125, blue: 0.133, alpha: 1)
+    static let darkishPink = UIColor(red: 0.910, green: 0.251, blue: 0.341, alpha: 1)
+    static let greenBlue = UIColor(red: 0.039, green: 0.773, blue: 0.557, alpha: 1)
+    static let gunmetal = UIColor(red: 0.322, green: 0.349, blue: 0.373, alpha: 1)
+    static let orangeYellow = UIColor(red: 1.000, green: 0.663, blue: 0.020, alpha: 1)
+    static let paleGrey = UIColor(red: 0.922, green: 0.933, blue: 0.945, alpha: 1)
+    static let paleGrey2 = UIColor(red: 0.969, green: 0.969, blue: 0.976, alpha: 1)
+    static let periwinkle = UIColor(red: 0.545, green: 0.408, blue: 1.000, alpha: 1)
+    static let softBlue = UIColor(red: 0.325, green: 0.514, blue: 0.910, alpha: 1)
+    static let steelGrey = UIColor(red: 0.482, green: 0.522, blue: 0.557, alpha: 1)
+}
