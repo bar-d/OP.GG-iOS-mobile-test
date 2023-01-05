5eb1ff562aed37fddece537c11fd6397b939b47f
bard <schy2503@naver.com>
2023-01-05 11:04:28 +0900

    feat: Data 영역 DataMapping을 위한 SummonerResponseDTO 구현

---
+++ OPGG-iOS-mobile-test/OPGG-iOS-mobile-test/Data/SummonerResponseDTO.swift
@@ -0,0 +1,82 @@
+//
+//  SummonerResponseDTO.swift
+//  OPGG-iOS-mobile-test
+//
+//  Created by bard on 2023/01/05.
+//
+
+import Foundation
+
+struct SummonerResponseDTO: Decodable {
+    let summoner: SummonerDTO
+}
+
+extension SummonerResponseDTO {
+    struct SummonerDTO: Decodable {
+        let name: String
+        let level: Int
+        let profileImageURL: URL
+        let leagues: [LeagueDTO]
+        
+        private enum CodingKeys: String, CodingKey {
+            case name
+            case level
+            case profileImageURL = "profileImageUrl"
+            case leagues
+        }
+    }
+}
+
+extension SummonerResponseDTO.SummonerDTO {
+    struct LeagueDTO: Decodable {
+        let hasResults: Bool
+        let wins: Int
+        let losses: Int
+        let tierRank: TierRankDTO
+    }
+    
+    func toDomain() -> Summoner {
+        return .init(
+            name: name,
+            level: level,
+            profileImageURL: profileImageURL,
+            leagues: leagues.map { $0.toDomain() }
+        )
+    }
+}
+
+extension SummonerResponseDTO.SummonerDTO.LeagueDTO {
+    struct TierRankDTO: Decodable {
+        let name: String
+        let tier: String
+        let imageURL: URL
+        let leaguePoints: Int
+        
+        private enum CodingKeys: String, CodingKey {
+            case name
+            case tier
+            case imageURL = "imageUrl"
+            case leaguePoints = "lp"
+        }
+    }
+    
+    func toDomain() -> League {
+        return .init(
+            hasResults: hasResults,
+            wins: wins,
+            losses: losses,
+            tierRank: tierRank.toDomain()
+        )
+    }
+}
+
+extension SummonerResponseDTO.SummonerDTO.LeagueDTO.TierRankDTO {
+    func toDomain() -> TierRank {
+        return .init(
+            name: name,
+            tier: tier,
+            imageURL: imageURL,
+            leaguePoints: leaguePoints
+        )
+    }
+}
