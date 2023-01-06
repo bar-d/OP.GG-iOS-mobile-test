51e2835befaa8ff42f1400d8aa3dae81db3005fe
bard <schy2503@naver.com>
2023-01-07 04:32:07 +0900

    refactor: ViewModel 프로토콜 리팩토링
    - RxSwift 도입에 따른 리팩토링

--- OPGG-iOS-mobile-test/OPGG-iOS-mobile-test/Scene/Common/ViewModel/ViewModel.swift
+++ OPGG-iOS-mobile-test/OPGG-iOS-mobile-test/Scene/Common/ViewModel/ViewModel.swift
@@ -5,9 +5,13 @@
 //  Created by bard on 2023/01/06.
 //
 
+import RxSwift
+
 protocol ViewModel {
     associatedtype Input
     associatedtype Output
     
-    init(output: Output)
+    var disposeBag: DisposeBag { get set }
+    
+    func transfrom(input: Input) -> Output
 }
